d38da07c1d03237d3206f3ac8b4da9b0745370ef
sonkhuong <sonmasteryi@gmail.com>
2022-11-11 13:24:35 +0700

    Update model and mockup demo

--- app/src/main/java/com/dagoras/osunolock/adapter/LockManagerAdapter.kt
+++ app/src/main/java/com/dagoras/osunolock/adapter/LockManagerAdapter.kt
@@ -5,8 +5,12 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.RecyclerView
+import com.dagoras.osunolock.CurrentTime
+import com.dagoras.osunolock.MockupForDemo
+import com.dagoras.osunolock.R
 import com.dagoras.osunolock.databinding.ItemManagerLockBinding
 import com.dagoras.osunolock.list_manager.fragment.ListManagerLockFragmentDirections
+import com.dagoras.osunolock.model.Lock
 import com.dagoras.osunolock.model.LockManager
 
 class LockManagerAdapter(private val fragment: Fragment, private val list: ArrayList<LockManager>) :
@@ -26,12 +30,22 @@ class LockManagerAdapter(private val fragment: Fragment, private val list: Array
         RecyclerView.ViewHolder(binding.root) {
         fun bind(item: LockManager) {
             with(binding) {
-                itemTitle.text = item.name
-                itemStatus.text = if (item.status == 0) "Out of date" else "Activate"
-                itemContent.text = item.content
+                itemTitle.text = getLockById(item.id_lock)?.lockName
+                itemRule.text = when (item.typeManager) {
+                    1 -> "Admin"
+                    2 -> "User"
+                    else -> "Root Admin"
+                }
+                itemStatus.text =
+                    if (item.startDate!! < CurrentTime.date && item.endDate!! > CurrentTime.date) "Activated" else "Out of date"
+                itemContent.text = String.format(
+                    root.resources.getString(R.string.date),
+                    item.startDate,
+                    item.endDate
+                )
 
                 itemView.setOnClickListener {
-                    navigateToManagerLock(fragment, 0)
+                    navigateToManagerLock(fragment)
                 }
 
                 itemRemove.setOnClickListener {
@@ -41,7 +55,17 @@ class LockManagerAdapter(private val fragment: Fragment, private val list: Array
         }
     }
 
-    private fun navigateToManagerLock(fragment: Fragment, position: Int) {
+    private fun getLockById(id: Int): Lock? {
+        val listLock = MockupForDemo.listLock
+        for (i in listLock) {
+            if (id == i.id) {
+                return i
+            }
+        }
+        return null
+    }
+
+    private fun navigateToManagerLock(fragment: Fragment) {
         val action =
             ListManagerLockFragmentDirections.actionListManagerLockFragmentToManagerLockFragment()
         fragment.findNavController().navigate(action)
